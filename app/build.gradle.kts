9617e4d6e3c7d8c4c7479c5a330102e5a40bf7bc
tienanh2003 <tiesenynart@gmail.com>
2024-04-25 00:04:26 +0700

    Code tiếp

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,11 +37,17 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
-    implementation("org.pytorch:pytorch_android_lite:1.13.1")
-    implementation("org.pytorch:pytorch_android_torchvision_lite:1.13.1")
+    implementation("org.pytorch:pytorch_android_lite:2.1.0")
+    implementation("org.pytorch:pytorch_android_torchvision_lite:2.1.0")
 // Use the most recent version of Camerax, currently that is alpha86.
-//    implementation("androidx.camera:camera-core:1.8.0-alpha86")
-//    implementation("androidx.camera:camera-camera2:1.0.0-alpha86")
-    implementation("androidx.camera:camera-core:1.3.0")
-
+//    implementation("androidx.camera:camera-core:1.8.0-alpha05")
+//    implementation("androidx.camera:camera-camera2:1.0.0-alpha05")
+    var camerax_version = "1.1.0-rc02"
+    implementation("androidx.camera:camera-core:${camerax_version}")
+    implementation("androidx.camera:camera-camera2:${camerax_version}")
+    implementation("androidx.camera:camera-lifecycle:${camerax_version}")
+    implementation("androidx.camera:camera-view:${camerax_version}")
+    implementation("androidx.camera:camera-video:${camerax_version}")
+    implementation("androidx.camera:camera-extensions:${camerax_version}")
+    implementation("androidx.camera:camera-mlkit-vision:${camerax_version}")
 }
